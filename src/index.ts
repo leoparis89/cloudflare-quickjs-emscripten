a1b8ee30f930954d6594edda858292d498dd0a6d
Lev Kowalski <lev.kowalski@ahrefs.com>
2024-10-28 16:28:57 +0100

    poc

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,81 @@
-import { shouldInterruptAfterDeadline } from "quickjs-emscripten"
+import { shouldInterruptAfterDeadline } from "quickjs-emscripten";
 
-import type { QuickJSWASMModule } from 'quickjs-emscripten';
-import { newQuickJSWASMModule, DEBUG_SYNC as baseVariant, newVariant } from 'quickjs-emscripten';
-import cloudflareWasmModule from './DEBUG_SYNC.wasm';
-import cloudflareWasmModuleSourceMap from './DEBUG_SYNC.wasm.map.txt';
+import type { QuickJSWASMModule } from "quickjs-emscripten";
+import { Arena } from "quickjs-emscripten-sync";
+import {
+  newQuickJSWASMModule,
+  DEBUG_SYNC as baseVariant,
+  newVariant,
+} from "quickjs-emscripten";
+import cloudflareWasmModule from "./DEBUG_SYNC.wasm";
+import cloudflareWasmModuleSourceMap from "./DEBUG_SYNC.wasm.map.txt";
 
 const cloudflareVariant = newVariant(baseVariant, {
-    wasmModule: cloudflareWasmModule,
-    wasmSourceMapData: cloudflareWasmModuleSourceMap,
+  wasmModule: cloudflareWasmModule,
+  wasmSourceMapData: cloudflareWasmModuleSourceMap,
 });
 
 let QuickJS: QuickJSWASMModule | undefined;
 
 addEventListener("fetch", async (event) => {
-    event.respondWith(serverResponse(event));
+  event.respondWith(serverResponse(event));
 });
 
-const serverResponse = async (event:FetchEvent) => {
-    QuickJS = await newQuickJSWASMModule(cloudflareVariant);
-    const result = QuickJS.evalCode("1 + 1", {
-        shouldInterrupt: shouldInterruptAfterDeadline(Date.now() + 1000),
-        memoryLimitBytes: 1024 * 1024,
-    })
+const input = `
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World</title>
+    <link rel="stylesheet" href="styles.css"> <!-- Link to external CSS if needed -->
+</head>
+<body>
+    <h1>Hello World</h1>
+</body>
+</html>
+`;
 
-    const body = JSON.stringify({result}, null, 2)
-    const headers =  {'content-type': 'application/json'}
-    const response = new Response(body, {headers})
-    return response
-}
+const serverResponse = async (event: FetchEvent) => {
+  QuickJS = await newQuickJSWASMModule(cloudflareVariant);
+
+  const ctx = QuickJS.newContext();
+  const arena = new Arena(ctx, { isMarshalable: true });
+
+  let result = "";
+
+  const handleResult = ctx.newFunction("handleResult", (r) => {
+    console.log(ctx.getString(r));
+    result = ctx.getString(r);
+  });
+
+  ctx.setProp(ctx.global, "onResult", handleResult);
+  handleResult.dispose();
+
+  const exposed = {
+    rewritter: new HTMLRewriter(),
+    input,
+  };
+
+  arena.expose(exposed);
+
+  arena.evalCode(`
+    // This works
+    onResult("result");
+
+    class ElementHandler {
+      async element(element) {
+    }
+    };
+
+   // rewritter.on fails with Maximum call stack size exceeded
+  //  rewritter.on("title", new ElementHandler());
+  `);
+
+  arena.dispose();
+  ctx.dispose();
+
+  const headers = { "content-type": "text/html" };
+  const response = new Response(input, { headers });
+  return response;
+};
